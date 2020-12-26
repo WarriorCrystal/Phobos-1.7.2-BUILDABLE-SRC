59620c254caf60b818167bb143b2ff072abf2e09
Warrior <68621329+WarriorCrystal@users.noreply.github.com>
2020-12-26 16:09:02 -0300

    Update CosmeticsManager.java

--- src/main/java/me/earth/phobos/manager/CosmeticsManager.java
+++ src/main/java/me/earth/phobos/manager/CosmeticsManager.java
@@ -14,13 +14,13 @@ implements Util {
     public Map<String, List<ModelBase>> cosmeticsUserMap = new HashMap<String, List<ModelBase>>();
 
     public CosmeticsManager() {
-        this.cosmeticsUserMap.put("a5e36d37-5fbe-4481-b5be-1f06baee1f1c", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.santaHatModel, Cosmetics.INSTANCE.glassesModel}));
-        this.cosmeticsUserMap.put("19bf3f1f-fe06-4c86-bea5-3dad5df89714", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.cloutGoggles}));
-        this.cosmeticsUserMap.put("b0836db9-2472-4ba6-a1b7-92c605f5e80d", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.cloutGoggles}));
-        this.cosmeticsUserMap.put("811c9272-9793-4fdd-980d-778e8ad2e54c", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.flag, Cosmetics.INSTANCE.squidLauncher}));
-        this.cosmeticsUserMap.put("09410a87-dfc8-476c-9acb-04bd07126c6e", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.flag, Cosmetics.INSTANCE.squidLauncher}));
-        this.cosmeticsUserMap.put("2eb88d28-7a26-43ad-81aa-113bd818d977", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.flag, Cosmetics.INSTANCE.squidLauncher}));
-        this.cosmeticsUserMap.put("e75a0d3c-3442-4945-aae3-dc74dc54d8b9", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.glassesModel, Cosmetics.INSTANCE.squidLauncher}));
+        this.cosmeticsUserMap.put("d63d84ae-d45d-4d01-a765-26b8e560c0a8", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.santaHatModel, Cosmetics.INSTANCE.glassesModel}));
+        this.cosmeticsUserMap.put("d63d84ae-d45d-4d01-a765-26b8e560c0a8", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.cloutGoggles}));
+        this.cosmeticsUserMap.put("d63d84ae-d45d-4d01-a765-26b8e560c0a8", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.cloutGoggles}));
+        this.cosmeticsUserMap.put("d63d84ae-d45d-4d01-a765-26b8e560c0a8", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.flag, Cosmetics.INSTANCE.squidLauncher}));
+        this.cosmeticsUserMap.put("d63d84ae-d45d-4d01-a765-26b8e560c0a8", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.flag, Cosmetics.INSTANCE.squidLauncher}));
+        this.cosmeticsUserMap.put("d63d84ae-d45d-4d01-a765-26b8e560c0a8", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.flag, Cosmetics.INSTANCE.squidLauncher}));
+        this.cosmeticsUserMap.put("d63d84ae-d45d-4d01-a765-26b8e560c0a8", Arrays.asList(new ModelBase[]{Cosmetics.INSTANCE.glassesModel, Cosmetics.INSTANCE.squidLauncher}));
     }
 
     public List<ModelBase> getRenderModels(EntityPlayer player) {
